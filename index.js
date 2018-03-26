7849a02fe7101493b13fd3cd2a141430dce6ba0e
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-26 08:46:31 -0500

    Erased some unnecesarry comments

--- index.js
+++ index.js
@@ -36,47 +36,11 @@ function nameErrorHtml(name) {
 
 function validName() {
     const input = $('#username');
-    input.on('input', function (event) {
+    input.on('input', function(event) {
         nameErrorHtml(event.currentTarget.value);
     });
 }
 
-// ************************* USERNAME ***********************************
-
-function usernameMsg(username) {
-    var msg = [];
-    if (username.length < 8 || username.length > 16) {
-        msg.push('<li> Username must be between 8-16 characters long</li>');
-    }
-    return msg.join('');
-}
-
-// function userErrorHtml(username) {
-//     console.log('user');
-//     var group = $('#ChangedTheName')
-//         .parent()
-//         .closest('.form-group');
-//     const html = usernameMsg(username);
-//     $(group)
-//         .removeClass('has-error')
-//         .removeClass('has-success');
-//     $('#ChangedTheName-errors').html(html);
-//     if (html.length > 0) {
-//         $(group).addClass('has-error');
-//     } else {
-//         $(group).addClass('has-success');
-//     }
-//     return html;
-// }
-
-// function validUsername() {
-//     const input = $('#ChangedTheName');
-//     input.on('input', function(event) {
-//         userErrorHtml(event.currentTarget.value);
-//     });
-// }
-
-// ********************************* PASSWORD ************************
 function checkingLetters(password) {
     const checking = /[a-z]/;
     if (checking.test(password)) {
@@ -130,7 +94,7 @@ function pwErrorHtml(password) {
 
 function validPassword() {
     const input = $('#password');
-    input.on('input', function (event) {
+    input.on('input', function(event) {
         pwErrorHtml(event.currentTarget.value);
     });
 }
@@ -173,7 +137,7 @@ function emailErrorHtml(email) {
 
 function validEmail() {
     const input = $('#email');
-    input.on('input', function (event) {
+    input.on('input', function(event) {
         emailErrorHtml(event.currentTarget.value);
     });
 }
@@ -183,10 +147,6 @@ function validEmail() {
 function checkValidName() {
     return nameErrorHtml($('#username').val()).trim() === '';
 }
-
-// function checkValidUser() {
-//     return userErrorHtml($('#ChangedTheName').val()).trim() === '';
-// }
 
 function checkValidPassword() {
     return pwErrorHtml($('#password').val()).trim() === '';
@@ -214,119 +174,36 @@ function checkSignInForm() {
     }
 }
 
-$('#signInForm').on('submit', function (event) {
+$('#signInForm').on('submit', function(event) {
     event.preventDefault();
     checkSignInForm();
 });
 
-// ******************************* SERVER *********************************
-
-function draw() {
-    $('body').html(PAGE_DATA.chirper.name);
-}
-
-// function signup(name, username, email, password) {
-//     $.post(
-//         'http://bcca-chirper.herokuapp.com/api/signup/',
-//         JSON.stringify({
-//             name: name,
-//             username: username,
-//             email: email,
-//             password: password
-//         })
-//     )
-//         .then()
-//         .catch();
-// }
-
-// ****************************** END OF SIGN UP ********************************
-
-var PAGE_DATA = {};
-
-// function postServer() {
-//     $('#signInForm').on('submit', function(event) {
-//         $.post(
-//             'https://bcca-chirper.herokuapp.com/api/signup/',
-//             JSON.stringify({
-//                 name: $('#username').val(),
-//                 username: $('#ChangedTheName').val(),
-//                 email: $('#email').val(),
-//                 password: $('#password').val()
-//             })
-//         )
-//             .then(function successulSignup(data) {
-//                 console.log(data);
-//             })
-//             .catch(function unsuccessfulSignup(response) {
-//                 console.log(response.status);
-//                 console.log(response.response.JSON);
-//             });
-//     });
-// }
-
-// ****************************** BEGINING OF SIGN IN *******************************
-
-// function logIn() {
-//     $('#logInForm').on('submit', function(event) {
-//         event.preventDefault();
-//         $.post(
-//             'https://bcca-chirper.herokuapp.com/api/login/',
-//             JSON.stringify({
-//                 username: $('#ChangedTheName').val(),
-//                 password: $('#Password').val()
-//             })
-//         )
-//             .then(function successfulLogIn(date) {
-//                 console.log('Hello World');
-//                 window.location = 'profile/profile.html';
-//                 console.log(data);
-//             })
-//             .catch(function unsuccessfulSignUp(response) {
-//                 // console.log(response.status);
-//                 // console.log(response.response.JSON);
-//             });
-//     });
-// }
-
-
-
-
-
 function loadPages() {
-    $('#Sign-Up').click(function () {
+    $('#Sign-Up').click(function() {
         $('#Store').hide();
         $('#signup-Page').show();
     });
-
-    $('#back').click(function () {
+    $('#back').click(function() {
         $('#Store').show();
         $('#signup-Page').hide();
     });
-    $('#Log-In').click(function () {
+    $('#Back').click(function() {
+        $('#Store').show();
+        $('#signup-Page').hide();
+        $('#login-Page').hide();
+    });
+    $('#Log-In').click(function() {
         $('#Store').hide();
         $('signup-Page').hide();
         $('#login-Page').show();
-    })
+    });
 }
 
 function main() {
     validName();
-    // validUsername();
     validPassword();
     validEmail();
-    // postServer();
-    // logIn();
-    // $('#signinform').on('submit', function(event) {
-    //     event.preventDefault();
-    //     var name = $('#username').val();
-    //     console.log(name);
-    //     var username = $('#ChangedTheName').val();
-    //     console.log(username);
-    //     var email = $('#email').val();
-    //     console.log(email);
-    //     var password = $('#password').val();
-    //     console.log(password);
-    // });
     loadPages();
 }
 
