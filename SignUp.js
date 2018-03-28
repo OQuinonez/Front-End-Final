22162aabbcb4c5c7ea45f38d8ddb778c7bcc23ed
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-28 10:33:34 -0500

    Changed the function CreateUser to have it working

--- SignUp.js
+++ SignUp.js
@@ -1,32 +1,28 @@
-function newUser() {
+function CreateNewUser() {
     $.ajax({
-        url: 'http://localhost:8080/newUser',
-        method: 'Post',
-        dataType: 'json',
-        crossDomain: true,
-        data: JSON.stringify({
-            UserName: $('#username').val(),
-            Address: $('#Address').val(),
-            Email: $('#email').val(),
-            password: $('#password').val()
-        }),
-        error: function(data, status, er) {
-            alert('status: ' + status);
-        }
-    }).then(function handleStoreResponse(response) {
-        console.log(response);
-        var products = response;
-        $('body').prepend(
-            "<div class='jumbotron'><center><h4>" +
-                response.UserName +
-                '</h3></center></div>'
-        );
-    });
-}
+        url: 'http://localhost:8080/posts/SignUp/',
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify({
+        UserName: $('#username').val(),
+        UAddress: $('#Address').val(),
+        Email: $('#email').val(),
+        PassCode: $('#password').val()
+    }),
+    contentType: 'application/json',
+    mimeType: 'applicatioin/json'});
+
+    console.log(JSON.stringify({
+        UserName: $('#username').val(),
+        UAddress: $('#Address').val(),
+        Email: $('#email').val(),
+        PassCode: $('#password').val()
+    }),)
+
 
 $('#signInForm').on('submit', function(event) {
     event.preventDefault();
-    newUser();
+    CreateNewUser();
 });
 
 function main() {
