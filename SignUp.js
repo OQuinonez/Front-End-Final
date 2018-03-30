c7c9b453ceab3ec7211f9835a647ace2eb9b5580
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-30 12:01:59 -0500

    Maded the logIn function which will help me log in a user

--- SignUp.js
+++ SignUp.js
@@ -32,9 +32,44 @@ function CreateNewUser() {
         });
 }
 
+function logIn() {
+    console.log(
+        JSON.stringify({
+            UserName: $('#username').val(),
+            UAddress: $('#Address').val(),
+            Email: $('#email').val(),
+            Password: $('#password').val()
+        })
+    );
+    fetch('http://localhost:8080/LogIn/', {
+        method: 'POST',
+        body: JSON.stringify({
+            Email: $('#email').val(),
+            pass_word: $('#password').val()
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        dataType: 'json',
+        crossDomain: true
+    })
+        .then(function handleResponse(response) {
+            var DATA = response;
+            console.log(DATA);
+        })
+        .catch(function handleError(error) {
+            console.log(error);
+        });
+}
+
 $('#signInForm').on('submit', function(event) {
     event.preventDefault();
     CreateNewUser();
+});
+
+$('#logInForm').on('submit', function(event) {
+    event.preventDefault();
+    logIn();
 });
 
 function main() {
