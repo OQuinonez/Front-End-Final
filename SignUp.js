f7503eb8d72bd2a73d695391a1cc62a99a14da1f
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-04-10 13:36:28 -0500

    Changed the values of the inputs to empty strings incase the user wants to go back to the sign up page after they have logged out

--- SignUp.js
+++ SignUp.js
@@ -71,6 +71,52 @@ function logIn() {
         });
 }
 
+function getParameterByUserName(username) {
+    var url = window.location.href;
+    username = username.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + username + '(=([^&#]*)|&|#|$)');
+    results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function logout() {
+    var UserName = getParameterByUserName('username');
+    fetch('http://localhost:8080/logout/' + UserName, {
+        method: 'POST',
+        dataType: 'json',
+        crossDomain: true,
+        contentType: 'applicatioin/json',
+        error: function(data, status, er) {
+            alert('status: ' + status);
+        }
+    })
+        .then(function handleResponse(response) {
+            $('#username')
+                .val('')
+                .val();
+            $('#Address')
+                .val('')
+                .val();
+            $('#email')
+                .val('')
+                .val();
+            $('#password')
+                .val('')
+                .val();
+
+            $('#Store').show();
+            $('#signup-Page').hide();
+            $('#login-Page').hide();
+            $('#feed-div').hide();
+            console.log('Did the correct thing');
+            // var DATA = response;
+            // console.log(DATA);
+        })
+        .catch(function catchError(error) {});
+}
+
 $('#signInForm').on('submit', function(event) {
     event.preventDefault();
     CreateNewUser();
@@ -78,11 +124,12 @@ $('#signInForm').on('submit', function(event) {
 
 $('#logInForm').on('submit', function(event) {
     event.preventDefault();
+    logIn();
     console.log('It does Something');
     $('#Store').hide();
-    $('signup-Page').hide();
-    $('#login-Page').show();
-    logIn();
+    $('#signup-Page').hide();
+    $('#login-Page').hide();
+    $('#feed-div').show();
 });
 
 function main() {
