bc4d91128ff820d65dc312dbdea0fb9a74c9f4d8
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-04-10 09:00:27 -0500

    Modified the logIn function to try to get it to work

--- SignUp.js
+++ SignUp.js
@@ -35,23 +35,32 @@ function CreateNewUser() {
 function logIn() {
     console.log(
         JSON.stringify({
-            UserName: $('#username').val(),
-            UAddress: $('#Address').val(),
-            Email: $('#email').val(),
-            Password: $('#password').val()
+            Email: $('#LogIn-email').val(),
+            Password: $('#Logpassword').val()
         })
     );
-    fetch('http://localhost:8080/LogIn/', {
+    fetch('http://localhost:8080/login', {
         method: 'POST',
+        dataType: 'json',
         body: JSON.stringify({
-            Email: $('#email').val(),
-            pass_word: $('#password').val()
+            email: $('#LogIn-email').val(),
+            hashedPassword: $('#Logpassword').val()
+        }),
+        crossDomain: true,
+        data: JSON.stringify({
+            Email: $('#LogIn-email').val(),
+            pass_word: $('#Logpassword').val()
         }),
         headers: {
             'Content-Type': 'application/json'
         },
+        // contentType: 'application/json',
+        // mimeType: 'application/json',
         dataType: 'json',
         crossDomain: true
+        // error: function(data, status, er) {
+        //     alert('UserName or Password incorrect please try again!!');
+        // }
     })
         .then(function handleResponse(response) {
             var DATA = response;
@@ -69,11 +78,16 @@ $('#signInForm').on('submit', function(event) {
 
 $('#logInForm').on('submit', function(event) {
     event.preventDefault();
+    console.log('It does Something');
+    $('#Store').hide();
+    $('signup-Page').hide();
+    $('#login-Page').show();
     logIn();
 });
 
 function main() {
     // CreateNewUser();
+    // logIn();
 }
 
 $(main);
