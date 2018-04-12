9bb2f030f71c488eb46ddeff0e29d2010ed29472
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-04-12 09:17:28 -0500

    Added the function showInvnetory to display my inventory to the user

--- Products.js
+++ Products.js
@@ -1,24 +1,28 @@
 function inventoryDisplay(products) {
+    console.log(products);
     var realInventory = products
         .map(function(inventory) {
             return [
                 '<div class="col-lg-4 col-md-4 col-sm-4 products">' +
                     '<img src="' +
-                    inventory.img +
+                    inventory.PicAddress +
                     '">' +
                     '<p></p>' +
                     '<b>Name: </b>' +
-                    inventory.name +
+                    inventory.ItemName +
                     '<br>' +
-                    '<b>Description: </b>' +
-                    inventory.description +
-                    '<br>' +
+                    // '<b>Description: </b>' +
+                    // inventory.description +
+                    // '<br>' +
                     '<b>Price: $</b>' +
-                    inventory.price +
+                    inventory.Price +
                     '<br>' +
                     '<b>Quantity: </b>' +
-                    inventory.quantity +
-                    '<p></p>'
+                    inventory.Quantity +
+                    '<p></p>',
+                '<button type="button" class="btn btn-primary" onclick="">' +
+                    '<i class="fa fa-shopping-cart" aria-hidden="true"></i>     Buy Item</button>',
+                '</div>'
                 // if (inventory.quatity > 0) {
                 //     html +=
                 //         '<button type="button" class="btn btn-primary" onclick="takeAway(' +
@@ -33,17 +37,27 @@ function inventoryDisplay(products) {
         })
         .join('');
 
-    return '<h3> Here Are our Products</h3>' + realInventory;
+    realInventory = '<h3> Here Are our Products</h3>' + realInventory;
+    $('#DisplayInventory').html(realInventory);
 }
 
-function initializeInventory(inventory) {
-    $('#DisplayInventory').html(inventoryDisplay(inventory));
-}
+// function initializeInventory(inventory) {
+//     $('#DisplayInventory').html(inventoryDisplay(inventory));
+// }
 
 function showInventory() {
-    fetch('http://localhost:8080/products')
-        .then(response => response.json())
-        .then(initializeInventory);
+    fetch('http://localhost:8080/products').then(response =>
+        response
+            .json()
+            .then(data => ({
+                data: data,
+                status: response.status
+            }))
+            .then(res => {
+                inventoryDisplay(res.data);
+            })
+    );
+    // .then(initializeInventory());
 }
 
 function main() {
